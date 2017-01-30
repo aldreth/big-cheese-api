cd6b29ed9dd538fb17f5a9c3a9cc1cbbfb4b0f29
Edward Andrews-Hodgson <edward@aldreth.com>
2017-01-30 20:33:22 +0000

    Only reveal create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :grant_applications
+  resources :grant_applications, only: :create
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
