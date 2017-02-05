c21b70fd1777a218a071409a1dd129f6b531652e
Edward Andrews-Hodgson <edward@aldreth.com>
2017-02-05 20:58:51 +0000

    Add & test validations

--- app/models/grant_application.rb
+++ app/models/grant_application.rb
@@ -1,2 +1,3 @@
 class GrantApplication < ApplicationRecord
+  validates :amount, :description, :name, :postcode, presence: true
 end
--- test/models/grant_application_test.rb
+++ test/models/grant_application_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class GrantApplicationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should validate_presence_of(:amount)
+  should validate_presence_of(:description)
+  should validate_presence_of(:name)
+  should validate_presence_of(:postcode)
 end
